a6b0c31fa9ed3b15eeebb781578e4838d76d64cd
Anshul Laddha <laddhaanshul@gmail.com>
2023-01-14 20:28:57 +0530

    feat: :sparkles: Understand how to return dynamic conent using template engines

---
+++ working_with_template_engines/app.js
@@ -0,0 +1,35 @@
+const path = require("path");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const expressHbs = require("express-handlebars");
+
+const app = express();
+
+//ejs template engine setup
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+//pug template engine setup
+// app.set('view engine', 'pug');
+// app.set('views', 'views');
+
+//handlebars template engine setup
+// app.engine("hbs", expressHbs);
+// app.set('view engine', 'hbs');
+// app.set('views', 'views');
+
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).render("404", { pageTitle: "Page Not Found", path: "" });
+});
+
+app.listen(3000);
